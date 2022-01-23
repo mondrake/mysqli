abc1e6c9d79ea972daee0003fa4b49209bf895fa
mondrake <mondrake.org@gmail.com>
2022-01-23 18:33:12 +0100

    fix gha

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -7,8 +7,7 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/1626.diff | git apply -v
 
 #3260007 Decouple Connection::query and MySql's Connection::getServerVersion from the wrapped PDO connection
-cat core/modules/mysqli/tests/github/3260007-4.patch
-git apply -v core/modules/mysqli/tests/github/3260007-4.patch
+curl https://www.drupal.org/files/issues/2022-01-23/3260007-4.patch | git apply -v
 
 #3259417 Missing typehints in test classes extending from Symfony
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/1682.diff | git apply -v
