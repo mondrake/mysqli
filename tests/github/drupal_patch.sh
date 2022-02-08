e31d756c72be28c72c3aa7ea3565315cd986368f
mondrake <mondrake.org@gmail.com>
2022-02-08 11:00:17 +0100

    Update patch for #3260007 Decouple Connection from the wrapped PDO connection to allow alternative clients

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -4,4 +4,4 @@
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/1626.diff | git apply -v
 
 #3260007 Decouple Connection from the wrapped PDO connection to allow alternative clients
-curl https://www.drupal.org/files/issues/2022-01-23/3260007-4.patch | git apply -v
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/1704.diff | git apply -v
