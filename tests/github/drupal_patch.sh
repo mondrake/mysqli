6d8c268df747a1fb41e681a67e73fdd9c1d75aaa
mondrake <mondrake.org@gmail.com>
2022-02-13 16:33:21 +0100

    One

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+#3110546 Allow contributed modules (mostly database drivers) to override tests in core
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
+
 #3256642 Autoload classes of database drivers modules' dependencies
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/1626.diff | git apply -v
 
