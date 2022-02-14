ba1f921093b91810c839a1e02faa311f83e75644
mondrake <mondrake.org@gmail.com>
2022-02-14 10:05:33 +0100

    aa

--- tests/src/Kernel/MysqliTransactionTest.php
+++ tests/src/Kernel/MysqliTransactionTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Drupal\KernelTests\Core\Database;
+namespace Drupal\Tests\mysqli\Kernel;
 
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Database\TransactionOutOfOrderException;
 use Drupal\Core\Database\TransactionNoActiveException;
-  use PHPUnit\Framework\Error\Warning;
-use Drupal\KernelTests\Core\Database\DatabaseTestBase;
+use PHPUnit\Framework\Error\Warning;
+use Drupal\KernelTests\Core\Database\TransactionTest as TransactionTestBase;
 
 /**
  * Tests the transaction abstraction system.
@@ -29,14 +29,12 @@ use Drupal\KernelTests\Core\Database\DatabaseTestBase;
  *
  * @group Database
  */
-class MysqliTransactionTest extends DatabaseTestBase {
-
+class MysqliTransactionTest extends TransactionTestBase {
   /**
    * Tests the compatibility of transactions with DDL statements.
    */
   public function testTransactionWithDdlStatement() {
     // First, test that a commit works normally, even with DDL statements.
-/*dump('****** ONE');
     $transaction = $this->connection->startTransaction();
     $this->insertRow('row');
     $this->executeDDLStatement();
@@ -44,7 +42,6 @@ class MysqliTransactionTest extends DatabaseTestBase {
     $this->assertRowPresent('row');
 
     // Even in different order.
-dump('****** TWO');
     $this->cleanUp();
     $transaction = $this->connection->startTransaction();
     $this->executeDDLStatement();
@@ -53,7 +50,6 @@ dump('****** TWO');
     $this->assertRowPresent('row');
 
     // Even with stacking.
-dump('****** THREE');
     $this->cleanUp();
     $transaction = $this->connection->startTransaction();
     $transaction2 = $this->connection->startTransaction();
@@ -66,7 +62,6 @@ dump('****** THREE');
     $this->assertRowPresent('row');
 
     // A transaction after a DDL statement should still work the same.
-dump('****** FOUR');
     $this->cleanUp();
     $transaction = $this->connection->startTransaction();
     $transaction2 = $this->connection->startTransaction();
@@ -78,7 +73,7 @@ dump('****** FOUR');
     unset($transaction3);
     unset($transaction);
     $this->assertRowAbsent('row');
-*/
+
     // The behavior of a rollback depends on the type of database server.
     if ($this->connection->supportsTransactionalDDL()) {
       // For database servers that support transactional DDL, a rollback
@@ -105,7 +100,6 @@ dump('****** FOUR');
       $this->assertRowAbsent('row');
     }
     else {
-dump('****** FIVE');
       // For database servers that do not support transactional DDL,
       // the DDL statement should commit the transaction stack.
       $this->cleanUp();
@@ -117,6 +111,7 @@ dump('****** FIVE');
         // Rollback the outer transaction.
         $transaction->rollBack();
         // @see \Drupal\mysql\Driver\Database\mysql\Connection::rollBack()
+// @todo mysqli does not fail when rolling back and no transaction active.
 //        $this->fail('Rolling back a transaction containing DDL should produce a warning.');
       }
       catch (Warning $warning) {
@@ -126,185 +121,4 @@ dump('****** FIVE');
       $this->assertRowPresent('row');
     }
   }
-
-  /**
-   * Inserts a single row into the testing table.
-   */
-  protected function insertRow($name) {
-    $this->connection->insert('test')
-      ->fields([
-        'name' => $name,
-      ])
-      ->execute();
-  }
-
-  /**
-   * Executes a DDL statement.
-   */
-  protected function executeDDLStatement() {
-    static $count = 0;
-    $table = [
-      'fields' => [
-        'id' => [
-          'type' => 'serial',
-          'unsigned' => TRUE,
-          'not null' => TRUE,
-        ],
-      ],
-      'primary key' => ['id'],
-    ];
-    $this->connection->schema()->createTable('database_test_' . ++$count, $table);
-  }
-
-  /**
-   * Starts over for a new test.
-   */
-  protected function cleanUp() {
-    $this->connection->truncate('test')
-      ->execute();
-  }
-
-  /**
-   * Asserts that a given row is present in the test table.
-   *
-   * @param string $name
-   *   The name of the row.
-   * @param string $message
-   *   The message to log for the assertion.
-   *
-   * @internal
-   */
-  public function assertRowPresent(string $name, string $message = NULL): void {
-    if (!isset($message)) {
-      $message = new FormattableMarkup('Row %name is present.', ['%name' => $name]);
-    }
-    $present = (boolean) $this->connection->query('SELECT 1 FROM {test} WHERE [name] = :name', [':name' => $name])->fetchField();
-    $this->assertTrue($present, $message);
-  }
-
-  /**
-   * Asserts that a given row is absent from the test table.
-   *
-   * @param string $name
-   *   The name of the row.
-   * @param string $message
-   *   The message to log for the assertion.
-   *
-   * @internal
-   */
-  public function assertRowAbsent(string $name, string $message = NULL): void {
-    if (!isset($message)) {
-      $message = new FormattableMarkup('Row %name is absent.', ['%name' => $name]);
-    }
-    $present = (boolean) $this->connection->query('SELECT 1 FROM {test} WHERE [name] = :name', [':name' => $name])->fetchField();
-    $this->assertFalse($present, $message);
-  }
-
-  /**
-   * Tests transaction stacking, commit, and rollback.
-   */
-/*  public function testTransactionStacking() {
-    // Standard case: pop the inner transaction before the outer transaction.
-    $transaction = $this->connection->startTransaction();
-    $this->insertRow('outer');
-    $transaction2 = $this->connection->startTransaction();
-    $this->insertRow('inner');
-    // Pop the inner transaction.
-    unset($transaction2);
-    $this->assertTrue($this->connection->inTransaction(), 'Still in a transaction after popping the inner transaction');
-    // Pop the outer transaction.
-    unset($transaction);
-    $this->assertFalse($this->connection->inTransaction(), 'Transaction closed after popping the outer transaction');
-    $this->assertRowPresent('outer');
-    $this->assertRowPresent('inner');
-
-    // Pop the transaction in a different order they have been pushed.
-    $this->cleanUp();
-    $transaction = $this->connection->startTransaction();
-    $this->insertRow('outer');
-    $transaction2 = $this->connection->startTransaction();
-    $this->insertRow('inner');
-    // Pop the outer transaction, nothing should happen.
-    unset($transaction);
-    $this->insertRow('inner-after-outer-commit');
-    $this->assertTrue($this->connection->inTransaction(), 'Still in a transaction after popping the outer transaction');
-    // Pop the inner transaction, the whole transaction should commit.
-    unset($transaction2);
-    $this->assertFalse($this->connection->inTransaction(), 'Transaction closed after popping the inner transaction');
-    $this->assertRowPresent('outer');
-    $this->assertRowPresent('inner');
-    $this->assertRowPresent('inner-after-outer-commit');
-
-    // Rollback the inner transaction.
-    $this->cleanUp();
-    $transaction = $this->connection->startTransaction();
-    $this->insertRow('outer');
-    $transaction2 = $this->connection->startTransaction();
-    $this->insertRow('inner');
-    // Now rollback the inner transaction.
-    $transaction2->rollBack();
-    unset($transaction2);
-    $this->assertTrue($this->connection->inTransaction(), 'Still in a transaction after popping the outer transaction');
-    // Pop the outer transaction, it should commit.
-    $this->insertRow('outer-after-inner-rollback');
-    unset($transaction);
-    $this->assertFalse($this->connection->inTransaction(), 'Transaction closed after popping the inner transaction');
-    $this->assertRowPresent('outer');
-    $this->assertRowAbsent('inner');
-    $this->assertRowPresent('outer-after-inner-rollback');
-
-    // Rollback the inner transaction after committing the outer one.
-    $this->cleanUp();
-    $transaction = $this->connection->startTransaction();
-    $this->insertRow('outer');
-    $transaction2 = $this->connection->startTransaction();
-    $this->insertRow('inner');
-    // Pop the outer transaction, nothing should happen.
-    unset($transaction);
-    $this->assertTrue($this->connection->inTransaction(), 'Still in a transaction after popping the outer transaction');
-    // Now rollback the inner transaction, it should rollback.
-    $transaction2->rollBack();
-    unset($transaction2);
-    $this->assertFalse($this->connection->inTransaction(), 'Transaction closed after popping the inner transaction');
-    $this->assertRowPresent('outer');
-    $this->assertRowAbsent('inner');
-
-    // Rollback the outer transaction while the inner transaction is active.
-    // In that case, an exception will be triggered because we cannot
-    // ensure that the final result will have any meaning.
-    $this->cleanUp();
-    $transaction = $this->connection->startTransaction();
-    $this->insertRow('outer');
-    $transaction2 = $this->connection->startTransaction();
-    $this->insertRow('inner');
-    $transaction3 = $this->connection->startTransaction();
-    $this->insertRow('inner2');
-    // Rollback the outer transaction.
-    try {
-      $transaction->rollBack();
-      unset($transaction);
-      $this->fail('Rolling back the outer transaction while the inner transaction is active resulted in an exception.');
-    }
-    catch (TransactionOutOfOrderException $e) {
-      // Expected exception; just continue testing.
-    }
-    $this->assertFalse($this->connection->inTransaction(), 'No more in a transaction after rolling back the outer transaction');
-    // Try to commit one inner transaction.
-    unset($transaction3);
-
-    // Try to rollback one inner transaction.
-    try {
-      $transaction->rollBack();
-      unset($transaction2);
-      $this->fail('Trying to commit an inner transaction resulted in an exception.');
-    }
-    catch (TransactionNoActiveException $e) {
-      // Expected exception; just continue testing.
-    }
-    $this->assertRowAbsent('outer');
-    $this->assertRowAbsent('inner');
-    $this->assertRowAbsent('inner2');
-  }
-*/
-
 }
