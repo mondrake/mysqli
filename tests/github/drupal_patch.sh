ca82dd62cf591a93d9de878afc820ecd3868dc37
mondrake <mondrake.org@gmail.com>
2023-01-30 16:54:39 +0100

    Update drupal_patch.sh

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -3,8 +3,11 @@
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
-#3256642 Autoload classes of database drivers modules' dependencies
+#3256642 Introduce database driver extensions and autoload database drivers' dependencies
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/3169.diff | git apply -v
+
+#3265086 Fix memory usage regression in StatementWrapper iterator
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/1842.diff | git apply -v
 
 # Extra patch
 #git apply -v ./mysqli_staging/tests/github/extra_patch.patch
