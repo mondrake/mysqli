a0ea5739e2ab68d52e3ed129e70b9922ff875e06
mondrake <mondrake.org@gmail.com>
2023-02-03 18:19:54 +0100

    Update drupal_patch.sh

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -7,7 +7,7 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/3169.diff | git apply -v
 
 #3265086 Fix memory usage regression in StatementWrapper iterator
-curl https://git.drupalcode.org/project/drupal/-/merge_requests/1842.diff | git apply -v
+curl https://www.drupal.org/files/issues/2023-02-03/3265086-62.patch | git apply -v
 
 # Extra patch
 # git apply -v ./mysqli_staging/tests/github/extra_patch.patch
