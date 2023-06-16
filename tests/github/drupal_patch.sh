9af35d85cd5a171fcb99858f009a233ca42ba0d5
mondrake <mondrake.org@gmail.com>
2023-06-16 18:30:57 +0200

    patch for #3217531

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -6,5 +6,8 @@ curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git a
 #3347497 Introduce a FetchModeTrait to allow emulating PDO fetch modes
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/3676.diff | git apply -v
 
+#3217531 Deprecate usage of Connection::getDriverClass for some classes, and use standard autoloading instead
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/757.diff | git apply -v
+
 # Extra patch
 # git apply -v ./mysqli_staging/tests/github/extra_patch.patch
