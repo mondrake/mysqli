e577a6c3925f941489b3778acd979391ce83ad2b
mondrake <mondrake.org@gmail.com>
2023-08-30 14:08:44 +0200

    Update drupal_patch.sh

--- tests/github/drupal_patch.sh
+++ tests/github/drupal_patch.sh
@@ -3,5 +3,8 @@
 #3110546 Allow contributed modules (mostly database drivers) to override tests in core
 curl https://git.drupalcode.org/project/drupal/-/merge_requests/291.diff | git apply -v
 
+#3364706 Refactor transactions
+curl https://git.drupalcode.org/project/drupal/-/merge_requests/4101.diff | git apply -v
+
 # Extra patch
 # git apply -v ./mysqli_staging/tests/github/extra_patch.patch
